12e8443fb2754727d2c30d6e6a4117f1d5b5c181
SCBProd <scbproduktion@gmail.com>
2026-07-17 14:58:27 +0200

    1

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -8,18 +8,51 @@ export async function getCurrentUser(req, res) {
   res.status(200).json(user);
 }
 
-export async function updateAvatar(req, res) {
-  const { file, user } = req;
-  if (!req.file) {
-    throw createHttpError(400, "No File");
+
+export async function updateAvatar(req, res, next) {
+  try {
+    const { user } = req;
+
+    if (!req.file) {
+      throw createHttpError(400, "No File");
+    }
+
+    const result = await saveFileToCloudinary(
+      req.file.buffer,
+      user._id
+    );
+
+    if (!result || !result.secure_url) {
+      throw createHttpError(
+        500,
+        "Cloudinary upload failed"
+      );
+    }
+
+    const updatedUser = await User.findOneAndUpdate(
+      {
+        _id: user._id,
+      },
+      {
+        avatarUrl: result.secure_url,
+      },
+      {
+        returnDocument: "after",
+      }
+    );
+
+    if (!updatedUser) {
+      throw createHttpError(
+        404,
+        "User not found"
+      );
+    }
+
+    res.status(200).json({
+      url: updatedUser.avatarUrl,
+    });
+
+  } catch (error) {
+    next(error);
   }
-
-  const result = await saveFileToCloudinary(file.buffer, user._id);
-
-  const updatedUser = await User.findOneAndUpdate(
-    { _id: user._id },
-    { avatarUrl: result.secure_url },
-    { returnDocument: "after" }
-  );
-  res.status(200).json({ url: updatedUser.avatarUrl });
 }
